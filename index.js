0ff75c45a0657e18f2ec840155856cc2d2a674db
Tanvir Rudro <rudro987@gmail.com>
2024-04-23 14:40:01 +0600

    Get and post API created for cart items

--- index.js
+++ index.js
@@ -45,6 +45,15 @@ async function run() {
         res.send(result);
     });
 
+
+    // Cart related apis
+
+    app.get('/carts', async(req, res) => {
+      const email = req.query.email;
+      const query = { email: email };
+        const result = await cartsCollection.find(query).toArray();
+        res.send(result);
+    });
     app.post('/carts', async(req, res) => {
       const cartItem = req.body;
       const result = await cartsCollection.insertOne(cartItem);
